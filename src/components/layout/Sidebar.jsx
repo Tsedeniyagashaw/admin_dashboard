117cbb0e0d675f4150a5e7a70bd26ba46a818d2f
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-12-28 04:44:11 +0300

    Add header

---
+++ src/components/layout/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200/50 dark:border-slate-700/50
+    flex flex-col relative z-10 '>
+    <div className='p-6 border-b border-slate-200/50 dark:border-slate-700/50'>
+    <div className='flex items-center space-x-3'>
+        <div className='w-10 h-10 bg-linear-to-r from-blue-600 to-purple-600 rounded-xl'>
+
+        </div>
+
+    </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default Sidebar
