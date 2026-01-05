3469295a4cf5bd30ca93fdc96e214cd43a32d040
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2026-01-05 04:17:15 +0300

    update sidebar

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,12 +2,14 @@ import { Bell, ChevronDown, Filter, Menu, Plus, Search, Settings, Sun } from 'lu
 import React from 'react'
 import img from '/jane.jfif'
 
-const Header = () => {
+const Header = ({sideBarCollapsed, onToggleSidebar}) => {
   return (
     <div className='bg-white/80 dark:bg-slate-900/80 backdrop:blur-xl border-b border-slate-200/50 dark:border-slate-700/50 px-6 py-4'>
       <div className='flex items-center justify-between'>
         <div className='flex items-center space-x-4'>
-           <button className='p-2 rounded-lg text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-salte-800 transition-colors'>
+           <button 
+           className='p-2 rounded-lg text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors'
+           onClick={onToggleSidebar}>
              <Menu className='w-5 h-5' />
            </button>
 
@@ -93,3 +95,5 @@ className='w-8 h-8 rounded-full ring-2 ring-blue-500'
 export default Header
 
 
+
+//46
