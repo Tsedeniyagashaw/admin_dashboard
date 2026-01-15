b3dc684682581cf3deb71e01afbb51483b7ab2a1
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2026-01-16 01:46:39 +0300

    Add SalesChart

--- src/components/Dashboard/SalesChart.jsx
+++ src/components/Dashboard/SalesChart.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-
-import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'lucide-react'
+import React from "react";
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
 
 const data = [
-    {name:"Electronics", value: 45,color: "#3b82f6"},
-    {name:"Clothing", value: 30, color: "#8b5cf6"},
-    {name:"Books", value: 15, color:"#10b981"},
-    {name: "Other", value: 10, color: "#f59e0b"}
-]
+  { name: "Electronics", value: 45, color: "#3b82f6" },
+  { name: "Clothing", value: 30, color: "#8b5cf6" },
+  { name: "Books", value: 15, color: "#10b981" },
+  { name: "Other", value: 10, color: "#f59e0b" },
+];
 
 const SalesChart = () => {
   return (
-    <div className='bg-white dark:bg-slate-900
-     backdrop-blur-xl rounded-2xl p-6 border
-      border-slate-200/50 dark:border-slate-700/50'>
-        <div className='mb-6'>
-     <h3 className='text-lg font-bold
-      text-slate-800 dark:text-white'>
-        Sales by Category</h3>
-        <p className='text-sm text-slate-500 dark:text-slate-400'>
-            Production Distribution
+    <div className="bg-white dark:bg-slate-900 backdrop-blur-xl rounded-2xl p-6 border border-slate-200/50 dark:border-slate-700/50">
+      <div className="mb-6">
+        <h3 className="text-lg font-bold text-slate-800 dark:text-white">
+          Sales by Category
+        </h3>
+        <p className="text-sm text-slate-500 dark:text-slate-400">
+          Production Distribution
         </p>
+      </div>
 
-        </div>
-
-        <div className='h-80'>
-
-        </div>
-    
+      <div className="h-48">
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              outerRadius="80%"
+              innerRadius="50%"
+              paddingAngle={5}
+              stroke="none"
+            >
+              {data.map((entry, index) => (
+                <Cell key={index} fill={entry.color} />
+              ))}
+            </Pie>
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SalesChart
+export default SalesChart;
